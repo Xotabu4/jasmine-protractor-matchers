f9accaff236b7415138389c584af5a5df10a0361
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2017-04-07 15:21:30 +0300

    First version of typings.
    Requires
    /// <reference types="jasmine-protractor-matchers" />
    
    In spec file to make matchers visible.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
+/// <reference types="jasmine" />
+
 declare module jasmine {
-    interface Matchers {
+    interface Matchers<T> {
         toAppear(ms_timeout?: number, custom_message?: string): boolean;
         toDisappear(ms_timeout?: number, custom_message?: string): boolean;
     }
