58910a7a5704791d4f832b942e9982f64b7567e3
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2017-04-25 16:49:42 +0300

    Added overload for better params suggestion and updated docs
    Added overload for better params suggestion and updated docs

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,38 @@
 
 declare module jasmine {
     interface Matchers<T> {
-        toAppear(ms_timeout?: number, custom_message?: string): boolean;
-        toDisappear(ms_timeout?: number, custom_message?: string): boolean;
+        /**
+         * Matcher for asserting that element is present and visible.
+         * Should be applied to ElementFinder object only.
+         * 
+         * @param {number} timeout Timeout to wait for appear of element in milliseconds. Default is 3000 milliseconds
+         * @param {string} custom_error_message Custom error message to throw on assertion failure. Default message is - 'Element ELEMENT_LOCATOR was expected to be shown in TIMEOUT milliseconds but is NOT visible'
+         */
+        toAppear(timeout?: number, custom_error_message?: string): boolean;
+
+        /**
+         * Matcher for asserting that element is present and visible.
+         * Should be applied to ElementFinder object only.
+         * 
+         * @param {string} custom_error_message Custom error message to throw on assertion failure. Default message is - 'Element ELEMENT_LOCATOR was expected to be shown in TIMEOUT milliseconds but is NOT visible'
+         */
+        toAppear(custom_error_message?: string): boolean;
+
+        /**
+         * Matcher for asserting that element is not displayed on the page.
+         * Should be applied to ElementFinder object only.
+         *
+         * @param {number} timeout Timeout to wait for disappear of element in milliseconds. Default is 3000 milliseconds
+         * @param {string} custom_error_message Custom error message to throw on assertion failure. Default message is - 'Element ELEMENT_LOCATOR was expected NOT to be shown in TIMEOUT milliseconds but is visible'
+         */
+        toDisappear(timeout?: number, custom_error_message?: string): boolean;
+
+        /**
+         * Matcher for asserting that element is not displayed on the page.
+         * Should be applied to ElementFinder object only.
+         *
+         * @param {string} custom_error_message Custom error message to throw on assertion failure. Default message is - 'Element ELEMENT_LOCATOR was expected NOT to be shown in TIMEOUT milliseconds but is visible'
+         */
+        toDisappear(custom_error_message?: string): boolean;
     }
 }
